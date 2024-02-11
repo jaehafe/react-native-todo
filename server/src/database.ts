3880191891c5637fbe99c42ef10ac162a2ef2aff
Jaeha Lee <wogkdkrm112@gmail.com>
2024-02-11 20:31:00 +0900

    feat: server - shareTodo

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,4 +1,4 @@
-import mysql from 'mysql2';
+import mysql, { ResultSetHeader } from 'mysql2';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -12,8 +12,20 @@ const pool = mysql
   .promise();
 
 export async function getTodoById(id: number) {
-  const row = await pool.query(`SELECT * FROM todos WHERE id = ?`, [id]);
-  console.log('row>>', row);
+  const [row] = await pool.query(`SELECT * FROM todos WHERE id = ?`, [id]);
+
+  return row[0];
+}
+
+export async function shareTodo(todo_id: string, user_id: string, shared_with_id: string) {
+  const [result] = await pool.query<ResultSetHeader>(
+    `
+    INSERT INTO shared_todos (todo_id, user_id, shared_with_id)
+    VALUES(?, ?, ?);
+    `,
+    [todo_id, user_id, shared_with_id]
+  );
+  return result.insertId;
 }
 
 getTodoById(1);
