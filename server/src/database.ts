bc86cba5f08cf9b4f2a51154e1e2f0a79ba6de79
Jaeha Lee <wogkdkrm112@gmail.com>
2024-02-11 20:35:24 +0900

    feat: server - CRUD todo

--- server/src/database.ts
+++ server/src/database.ts
@@ -24,6 +24,11 @@ export async function getTodosByID(id: string) {
   return rows;
 }
 
+export async function getTodo(id: number) {
+  const [rows] = await pool.query(`SELECT * FROM todos WHERE id = ?`, [id]);
+  return rows[0];
+}
+
 export async function getTodoById(id: number) {
   const [row] = await pool.query(`SELECT * FROM todos WHERE id = ?`, [id]);
 
@@ -49,4 +54,44 @@ export async function getSharedTodoByID(id: string) {
 export async function getUserByID(id: string) {
   const [rows] = await pool.query(`SELECT * FROM users WHERE id = ?`, [id]);
   return rows[0];
+}
+
+export async function getUserByEmail(email: string) {
+  const [rows] = await pool.query(`SELECT * FROM users WHERE email = ?`, [email]);
+  return rows[0];
+}
+
+export async function createTodo(user_id: string, title: string) {
+  const [result] = await pool.query<ResultSetHeader>(
+    `
+    INSERT INTO todos (user_id, title)
+    VALUES (?, ?)
+  `,
+    [user_id, title]
+  );
+  const todoID = result.insertId;
+  return getTodo(todoID);
+}
+
+export async function deleteTodo(id) {
+  const [result] = await pool.query(
+    `
+    DELETE FROM todos WHERE id = ?;
+    `,
+    [id]
+  );
+  return result;
+}
+
+export async function toggleCompleted(id, value) {
+  const newValue = value === true ? 'TRUE' : 'FALSE';
+  const [result] = await pool.query(
+    `
+    UPDATE todos
+    SET completed = ${newValue} 
+    WHERE id = ?;
+    `,
+    [id]
+  );
+  return result;
 }
